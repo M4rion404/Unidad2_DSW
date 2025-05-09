0f005dd117fb220fe17e6ad4962ea59faae706fd
Marion <lumajimenezla@ittepic.edu.mx>
2025-05-09 08:25:33 -0700

    Elimine la carpeta 02GraphQl porque me falto algo

--- 02GraphQl/index.js
+++
@@ -1,9 +0,0 @@
-const { ApolloServer } = require('apollo-server');
-const typeDefs = require('./schemas/typeDefs.js');
-const resolvers = require('./controllers/userController.js');
-
-const server = new ApolloServer({ typeDefs, resolvers });
-
-server.listen().then(({ url }) => {
-    console.log(`Servidor corriendo en `+url);
-});
